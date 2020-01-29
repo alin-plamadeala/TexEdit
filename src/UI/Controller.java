b6ae6c006528fa4f81552df83450a675c3cb75ce
alinp <alin.plamadeala@outlook.com>
2020-01-29 09:47:03 +0100

    - test textarea

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -1,4 +1,15 @@
 package UI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    @FXML
+    private TextArea areaText;
+
+    @FXML
+    private void onTest() {
+        System.out.println(areaText.getText());
+    }
+
 }
